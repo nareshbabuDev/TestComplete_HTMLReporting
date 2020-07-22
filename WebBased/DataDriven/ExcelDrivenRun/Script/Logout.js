b98a1c0e9d754562ac409b72d8743e6ff2f57cc8
nareshbabuDev <52285851+nareshbabuDev@users.noreply.github.com>
2020-07-23 00:02:46 +0530

    Delay function_Logout
    Logout

--- WebBased/DataDriven/ExcelDrivenRun/Script/Logout.js
+++ WebBased/DataDriven/ExcelDrivenRun/Script/Logout.js
@@ -5,7 +5,9 @@ function logout()
   let page = browser.pageOrangehrm;
   Logger.fn_createteststep("DONE","Click Logout");
   page.linkWelcome.Click();
+  aqUtils.Delay(5000);
   page.link.Click();
   browser.pageLogin.Wait();
+  aqUtils.Delay(5000);
   Logger.fn_createteststep("PASS","Logged Out by User:"+Project.Variables.Username);
 }
